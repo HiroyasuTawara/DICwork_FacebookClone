cb8db1ef48d99366348d29139e351cf8aed433a4
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-05 12:00:31 +0900

    Add: usersモデルにバリデーション設定、emailカラムにユニーク制約を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, 
+  length: { maximum: 30 }
+
+  validates :emaill, presence: true, 
+  length: { maximum: 255 }, 
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  before_validation { email.downcase! }
 end
